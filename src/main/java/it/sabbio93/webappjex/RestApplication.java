91b30b7e8809e63fbdf209da27be418456f8637d
andrea <sabbio.a93@gmail.com>
2017-12-08 14:56:48 +0100

    init

---
+++ src/main/java/it/sabbio93/webappjex/RestApplication.java
@@ -0,0 +1,9 @@
+package swarmWillyEJB;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class RestApplication extends Application 
+{
+} 
